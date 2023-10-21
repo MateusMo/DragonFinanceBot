65d0d516fe01d659af5a059fdfdf8341fb2e7b5e
MateusMo <mat.marq.oliveira@gmail.com>
2023-10-21 16:06:35 -0300

    First Commit

---
+++ DragonFinanceBot/Bot/Menu.cs
@@ -0,0 +1,26 @@
+﻿using DragonFinanceBot.AsciiArts;
+using DragonFinanceBot.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFinanceBot.Bot
+{
+    public class Menu
+    {
+        public static void ShowMenu()
+        {
+            var asciiImage
+                = Dragon.GetDragon();
+            System.Console.WriteLine(asciiImage);
+
+            var menu = FinanceMenu.ShowMenu();
+            System.Console.WriteLine(menu);
+
+            System.Console.ReadLine();
+
+        }
+    }
+}
---
+++ DragonFinanceBot/Menu/FinanceMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonFinanceBot.Menu
+{
+    public class FinanceMenu
+    {
+        public static string ShowMenu()
+        {
+            string menu = @"
+            1 - Buscar altas e baixas no infomoney
+            2 - Buscar preço de criptos na coinmaketcap
+            3 - Fogo do dragão no bear marketing
+";
+            return menu;
+        }
+    }
+}
